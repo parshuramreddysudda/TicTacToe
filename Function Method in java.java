732c55a35f5c34edb6d764f24ed4b80f466af873
Parshuram reddy <pandu@Parshurams-MacBook-Pro.local>
2017-12-21 23:56:35 +0530

    Game Build Started

---
+++ Function Method in java.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Temp{
+    public  void hell()
+    {
+
+        System.out.println("heller");
+    }
+    public void heller()
+    {
+        System.out.println("second Evolved");
+    }
+    public static void main(String args[])
+    {
+        Temp obj = new Temp();
+        obj.hell();
+        obj.heller();
+
+    }
+}
